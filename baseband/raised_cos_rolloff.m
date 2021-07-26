f8a4f26ad485165eddfb08a95e35e64ccbee7a67
donghui_laptop <2507441744@qq.com>
2021-07-26 17:11:02 +0800

    对升余弦滚降的频域特性进行仿真

--- baseband/raised_cos_rolloff.m
+++ baseband/raised_cos_rolloff.m
@@ -1,18 +1,18 @@
 %对升余弦滚降滤波器的特性研究（Rasied cosine roll-off filter）
 Tb = 1; %码元传输速率
-fs = 100; %100Hz的采样频率
+fs = 1000; %1000Hz的采样频率
 t = -5*Tb:1/fs:5*Tb-1/fs; %时域范围
 N = length(t);
 
 %f = (-N/2:N/2-1)*fs/N; %频域范围,但是fft之后的效果不好，这里我们直接根据频域的公式来绘制频域图
-
+f = -2/Tb:fs/N:2/Tb; %画图时显示的频率范围
 
 
 fn = 1/2*Tb; %理想低通滤波器的截止频率
 
 a = [0 0.5 0.75 1]; %滚降系数
 H = zeros(length(a),length(t));
-F = zeros(length(a),length(t));
+F = zeros(length(a),length(f));
 
 for i = 1:length(a)
     H(i,:) = rcos(a(i),Tb,t);
@@ -32,6 +32,33 @@ grid on;
 xlabel('时间t');
 ylabel('时域响应');
 
+%----升余弦特性低通信道的表达式----
+for i=1:length(a)
+    for j=1:length(f)
+        if abs(f(j))>(1+a(i))/(2*Tb)
+            F(i,j)=0;
+        elseif abs(f(j))<(1-a(i))/(2*Tb)
+            F(i,j)=Tb;
+        else
+            F(i,j)=0.5*Tb*(1+cos(pi*Tb/(a(i)+eps)*(abs(f(j))-(1-a(i))/(2*Tb))));
+        end
+    end
+end
+
+figure('NumberTitle', 'off', 'Name','升余弦滚降特性信道的频域响应');
+plot(f,F(1,:));
+hold on;
+plot(f,F(2,:));
+hold on;
+plot(f,F(3,:));
+hold on;
+plot(f,F(4,:));
+hold on;
+legend('a=0','a=0.5','a=0.75','a=1','Fontsize',15);
+grid on;
+xlabel('频率f');
+ylabel('频域响应');
+axis([-2 2 0 1.2])
 % figure('NumberTitle', 'off', 'Name','升余弦滚降特性信道的频域响应');
 % F1 = abs(fft(H(1,:)));
 % F1 = 1/N*fftshift(F1);
