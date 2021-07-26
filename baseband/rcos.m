62c3831fb571466cecff1fd8fdec1896f75eb938
donghui_laptop <2507441744@qq.com>
2021-07-26 17:46:32 +0800

    修改显示效果

--- baseband/rcos.m
+++ baseband/rcos.m
@@ -1,6 +1,6 @@
 function [ht] = rcos(a,Tb,t)
 %temp = 10^(-5); %防止分母为零
-%ht = sinc(t/Tb).*(cos(pi*a/Tb*t)+temp)./(1-4*a^2/Tb^21*t.^2+temp);
-ht = sinc(t/Tb).*(cos(pi*a/Tb*t))./(1-4*a^2/Tb^21*t.^2);
+%ht = sinc(t/Tb).*(cos(pi*a/Tb*t)+temp)./(1-4*a^2/Tb^2*t.^2+temp);
+ht = sinc(t/Tb).*(cos(pi*a/Tb*t))./(1-4*a^2/Tb^2*t.^2);
 end
 
